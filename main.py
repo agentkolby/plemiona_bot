a8178e65ef6b77c6334b61284812ca04d7f3f394
Szymon Kolber <kolberszymon@gmail.com>
2021-07-24 12:07:07 +0200

    Initalization

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('world')
+import time
+import os
+
+from dotenv import load_dotenv 
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+load_dotenv()
+
+USER = os.environ.get("USERNAME")
+
+print(USER)
+
+driver = webdriver.Chrome('drivers/mac_chromedriver')
+
+driver.get("https://www.plemiona.pl")
+time.sleep(5)
+driver.close()
